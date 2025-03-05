69d85393b50ee234e767687d4101ad46f9ed7807
akydev <akydev18@gmail.com>
2025-03-05 17:10:18 +0530

    added view appointment skeleton

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,14 +4,16 @@ import DoctorList from "../Components/DoctorList";
 import SpecialtyList from "../Components/SpecialtyList";
 import ViewAppointment from "../Components/ViewAppointment";
 import SpecialtySkeleton from "../skeleton/SpecialtySkeleton";
+import ViewAppointmentSkeleton from "../skeleton/ViewAppointmentSkeleton";
 
 function page() {
   return (
     <>
       {/* <ViewAppointment /> */}
+      <ViewAppointmentSkeleton />
       {/* <DoctorList /> */}
-      <SpecialtySkeleton />
-      <SpecialtyList />
+      {/* <SpecialtySkeleton /> */}
+      {/* <SpecialtyList /> */}
     </>
   );
 }
